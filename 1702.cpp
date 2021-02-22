b6f38645f54373c559a98853b2b53c5aab35b714
Sella1693 <s21910257@student.unklab.ac.id>
2021-02-22 14:44:54 +0800

    new file

---
+++ 1702.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+    int main (){
+        string nama;
+
+        cout << "Stevali Item";
+        getline(cin, nama);
+
+        cout << "Nama anda adalah: " << nama << endl;
+
+        return 0;
+    }
